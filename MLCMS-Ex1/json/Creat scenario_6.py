d700596d9f6a65a963055c51e74f2cada081d4fe
Chuck00027 <102159768+Chuck00027@users.noreply.github.com>
2023-04-30 15:08:59 +0200

    MLCMS Ex1
    Exercise sheet 1: Modeling of human crowds
    
    In this exercise, you will learn how to model and simulate a human crowd with a simple cellular automaton.In general, cellular automata are a computational tool to model a complex system , i.e. a system that is built up of many interacting parts.

---
+++ MLCMS-Ex1/json/Creat scenario_6.py
@@ -0,0 +1,33 @@
+import json
+
+
+"""
+Here build up the scenario_6 , and export to a json file
+"""
+
+# create an empty dictionary to store positions
+positions = {"scenario":6,
+            "width": 24,
+            "height": 24,
+            "target": [], 
+             "obstacles": [], 
+             "pedestrians": []}
+
+# targets positions 
+for i in range(20, 24):        
+    positions["target"].append( [i,0] )
+
+# obstacle positions
+for i in range(0,20):
+    for j in range(0, 20):
+        positions["obstacles"].append( [i,j] )
+
+# pedestrians positions (20 pedestrians)
+for i in range(0, 5):
+    for j in range(20, 24):
+        positions["pedestrians"].append( [i,j,1] )
+
+# write positions to json file
+with open("scenario-888.json", "w") as f:
+    json.dump(positions, f)
+
